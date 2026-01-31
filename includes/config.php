b7cf066e49fc457a8e2a261536747e4adad9f89e
Aditya981920 <aditya981920@gmail.com>
2026-01-31 21:26:02 +0530

    Config.php update

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 define('DB_HOST', 'bdsn0q42sxbm1wlpmpae-mysql.services.clever-cloud.com');
 define('DB_NAME', 'bdsn0q42sxbm1wlpmpae');
 define('DB_USER', 'uswz2latdyj49um0');
-define('DB_PASS', '08pjRXsfMPmvZJTnyLTE'); // 👈 paste exact password
+define('DB_PASS', '08pjRXsfMPmvZJTnyLTE');
 
 try {
     $dbh = new PDO(
@@ -11,10 +11,7 @@ try {
         DB_PASS,
         [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-
-            // ✅ CORRECT WAY FOR CLEVER CLOUD
-            PDO::MYSQL_ATTR_SSL_CA => null,
-            PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         ]
     );
 } catch (PDOException $e) {
